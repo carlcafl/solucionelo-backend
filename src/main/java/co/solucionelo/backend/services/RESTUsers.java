2ff0b4f64cd360244042684d2481c3dd48f8bef1
carlcafl <ccarmona@gmail.com>
2016-10-11 13:40:20 -0500

    Bug CORS 3

--- src/main/java/co/solucionelo/backend/services/RESTUsers.java
+++ src/main/java/co/solucionelo/backend/services/RESTUsers.java
@@ -25,6 +25,7 @@ public class RESTUsers extends RESTService {
 	public Response doOptions() {
 		//processResponse();
 		return Response.ok()
+				.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 				.header("Access-Control-Allow-Origin", "*")
 				.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
 				.build();
