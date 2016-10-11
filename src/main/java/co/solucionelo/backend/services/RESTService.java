95dd88ad8ae29df8a07c38af9cf41bcdb508c9f1
carlcafl <ccarmona@gmail.com>
2016-10-11 17:42:21 -0500

    Bug CORS 7

--- src/main/java/co/solucionelo/backend/services/RESTService.java
+++ src/main/java/co/solucionelo/backend/services/RESTService.java
@@ -12,11 +12,11 @@ public abstract class RESTService {
 	protected HttpServletRequest  request;
 
 	protected void processResponse() {
-		String reqHead = request.getHeader("Access-Control-Request-Headers");		 
-        /*if(null != reqHead && !reqHead.equals(""))
+		/*String reqHead = request.getHeader("Access-Control-Request-Headers");		 
+        if(null != reqHead && !reqHead.equals(""))
         	response.addHeader("Access-Control-Allow-Headers", reqHead);
         else*/
-        	response.addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        	response.addHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Auth-Token, Accept, Key, X-Requested-With");
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.addHeader("Access-Control-Allow-Methods",
 				"GET, POST, PUT, DELETE, OPTIONS, HEAD");
